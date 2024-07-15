1ba689fe74ba7f7c6aee9f3ddd64f04fa27833cd
文珍 <wenzhen@yuanxinjituan.com>
2024-07-15 16:46:45 +0800

    chore: 打包配置

--- components/KeepAlive/KeepAlive.tsx
+++ components/KeepAlive/KeepAlive.tsx
@@ -4,17 +4,19 @@ import KeepContext from './KeepContext'
 import useMemoizedFn from '../hooks/useMemoizedFn'
 import { execActivated, execDeactivated } from './useLifeCycle'
 
-interface IKeepAliveProps {
+export interface IKeepAliveProps {
   // 缓存键值
   cacheKey: string
 }
 
+type IKeeperElement = HTMLDivElement | null
+
 const prevRefs: {
-  [key: string]: HTMLDivElement | null
+  [key: string]: IKeeperElement
 } = {}
 
 const KeepAlive: FC<PropsWithChildren<IKeepAliveProps>> = (props) => {
-  const keepAliveRef = useRef<HTMLDivElement | null>(null)
+  const keepAliveRef = useRef<IKeeperElement>(null)
   const { push, getCache } = useContext(KeepContext)
 
   // 激活状态: 页面放入keep-alive中展示
@@ -24,13 +26,13 @@ const KeepAlive: FC<PropsWithChildren<IKeepAliveProps>> = (props) => {
     const pageContent = await push(props.cacheKey, props.children)
     // console.log('pageContent=====>', pageContent)
     // 将渲染后的realContent移动到KeepAlive中展示
-    if (keepAliveRef.current && pageContent) {
+    if (pageContent && keepAliveRef.current) {
       if (keepAliveRef.current.hasChildNodes()) {
         // console.log('replaceChild=====>')
         keepAliveRef.current.replaceChild(pageContent, keepAliveRef.current?.lastChild as ChildNode)
       } else {
         // console.log('appendChild=====>')
-        keepAliveRef.current?.appendChild(pageContent)
+        keepAliveRef.current.appendChild(pageContent)
       }
       execActivated(props.cacheKey)
       prevRefs[props.cacheKey] = keepAliveRef.current
@@ -38,11 +40,10 @@ const KeepAlive: FC<PropsWithChildren<IKeepAliveProps>> = (props) => {
   })
 
   // 缓存状态: 页面重新缓存入Keeper内
-  const deactivated = useMemoizedFn((_cacheKey: string, keepContainerElement: HTMLDivElement) => {
+  const deactivated = useMemoizedFn((_cacheKey: string, keepContainerElement: IKeeperElement) => {
     // 页面重新缓存入Keeper内
     const keeper = getCache(_cacheKey)
     // console.log('<============================= Start Of ==========================> ')
-    // console.log('Enter =====> cacheKey:', props.cacheKey)
     // console.log('Leave =====> cacheKey:', _cacheKey)
     // console.log('ref.current ====>', ref.current)
     // console.log('prevRef ====>', prevRefs[_cacheKey])
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
+import dts from 'vite-plugin-dts'
 
 function resolve(_path: string) {
   return path.resolve(__dirname, _path)
@@ -9,15 +10,28 @@ function resolve(_path: string) {
 const libName = 'react-keep-alive'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: resolve('./dist'),
-    sourcemap: true,
-    lib: {
-      entry: './components/index.ts', // 指定组件编译入口文件
-      name: libName,
-      fileName: libName,
+export default defineConfig(({ mode }) => {
+  if (mode === 'development') {
+    return {
+      plugins: [react()],
+    }
+  }
+  return {
+    plugins: [
+      react(),
+      dts({
+        outDir: './dist/types',
+        tsconfigPath: './tsconfig.lib.json',
+      }),
+    ],
+    build: {
+      outDir: resolve('./dist'),
+      sourcemap: true,
+      lib: {
+        entry: './components/index.ts', // 指定组件编译入口文件
+        name: libName,
+        fileName: libName,
+      },
     },
-  },
+  }
 })
